a70ffc3fd01902da8cdb9c81c0f16e4d79eca4d0
sebas <sebas@DESKTOP-S92RB8H>
2023-10-24 11:48:35 +0200

    Add project files.

---
+++ Radijvektori/Program.cs
@@ -0,0 +1,50 @@
+﻿using Radijvektori;
+
+Radijvektor a; // = new Radijvektor();
+a.X = 5; a.Y = 6; a.Z = -1; 
+Console.WriteLine(a.ToString());
+Radijvektor b = new Radijvektor();
+Console.WriteLine(b.ToString());
+Radijvektor c = default;
+Console.WriteLine(c.ToString());
+
+Radijvektor d;
+Unos(out d);
+Console.WriteLine(d.ToString());
+
+INorma e = d;
+d.Normiraj();
+Console.WriteLine(e.ToString());
+Console.WriteLine(d);
+
+/* Radijvektor[] radijvektori = new Radijvektor[10];
+foreach(Radijvektor v in radijvektori)
+{
+    Console.WriteLine(v);
+} */
+
+/* Temp d = new Temp();
+Console.WriteLine(d.R);
+internal class Temp
+{
+    public Radijvektor R;
+} */
+
+static void Unos(out Radijvektor a)
+{ 
+    while (true)
+    {
+        Console.Write("Unesite koordinate odvojene razmakom: ");
+        var ulaz = Console.ReadLine();
+        if (ulaz == null)
+            continue;
+        var koordinate = ulaz.Split(' ');
+        if (koordinate.Length != 3)
+            continue;
+        if (!double.TryParse(koordinate[0], out a.X)
+            || !double.TryParse(koordinate[1], out a.Y)
+            || !double.TryParse(koordinate[2], out a.Z))
+            continue;
+        break;
+    }
+}
---
+++ Radijvektori/Radijvektor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radijvektori
+{
+    internal interface INorma
+    {
+        double Norma {  get; }
+        void Normiraj();
+    }
+    internal /* ref readonly */ struct Radijvektor : INorma
+    {
+        public double Norma => Duljina;
+        public void Normiraj()
+        {
+            double n = Norma;
+            if(n != 0)
+            {
+                X /= n; Y /= n; Z /= n;
+            }
+        }
+        public /* readonly */ double X, Y, Z;
+        public readonly double Duljina
+        {
+            get { /* X = 5; */  return Math.Sqrt(X * X + Y * Y + Z * Z);}
+        }
+        public override string ToString()
+            => "(" + X + ", " + Y + ", " + Z + ")";
+        public Radijvektor(double x, double y = 0, double z = 0)
+        {
+            X = x; Y = y; Z = z;
+        }
+        public Radijvektor() : this(1) { }
+        public static Radijvektor operator + (Radijvektor a,  Radijvektor b)
+        {
+            return new Radijvektor(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+        public static Radijvektor operator * (double k, Radijvektor a)
+        {
+            return new Radijvektor(k * a.X, k * a.Y, k * a.Z);
+        }
+        public static Radijvektor operator -(Radijvektor a, Radijvektor b)
+            => a + (-1) * b;
+    }
+}
